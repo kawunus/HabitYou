2408beea57ff1e1badf3cf4a5c229b16e752cebf
kawunus <s.markovtsev@gmail.com>
2025-05-23 20:19:24 +0300

    Feat: Implement NoteInteractor and NoteInteractorImpl

--- app/src/main/java/com/kawunus/habitu/diary/domain/api/NoteInteractor.kt
+++ app/src/main/java/com/kawunus/habitu/diary/domain/api/NoteInteractor.kt
@@ -1,4 +1,15 @@
 package com.kawunus.habitu.diary.domain.api
 
+import com.kawunus.habitu.diary.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
 interface NoteInteractor {
+
+    suspend fun insertNote(note: Note)
+
+    suspend fun deleteNote(note: Note)
+
+    suspend fun updateNote(note: Note)
+
+    suspend fun getAllNotes(): Flow<List<Note>>
 }
--- app/src/main/java/com/kawunus/habitu/diary/domain/impl/NoteInteractorImpl.kt
+++ app/src/main/java/com/kawunus/habitu/diary/domain/impl/NoteInteractorImpl.kt
@@ -2,6 +2,29 @@ package com.kawunus.habitu.diary.domain.impl
 
 import com.kawunus.habitu.diary.domain.api.NoteInteractor
 import com.kawunus.habitu.diary.domain.api.NoteRepository
+import com.kawunus.habitu.diary.domain.model.Note
+import com.kawunus.habitu.utils.toNote
+import com.kawunus.habitu.utils.toNoteDto
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
-class NoteInteractorImpl(noteRepository: NoteRepository) : NoteInteractor {
+class NoteInteractorImpl(private val noteRepository: NoteRepository) : NoteInteractor {
+
+    override suspend fun insertNote(note: Note) {
+        noteRepository.insertNote(note.toNoteDto())
+    }
+
+    override suspend fun deleteNote(note: Note) {
+        noteRepository.deleteNote(note.toNoteDto())
+    }
+
+    override suspend fun updateNote(note: Note) {
+        noteRepository.updateNote(note.toNoteDto())
+    }
+
+    override suspend fun getAllNotes(): Flow<List<Note>> {
+        return noteRepository.getAllNotes().map { noteList ->
+            noteList.map { it.toNote() }
+        }
+    }
 }
