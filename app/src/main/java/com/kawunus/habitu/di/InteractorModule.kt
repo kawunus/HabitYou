da411a34f378a93a54f704ae1402cfd73282a853
kawunus <s.markovtsev@gmail.com>
2025-05-23 20:23:07 +0300

    Feat: Add Note-DI modules for note feat

--- app/src/main/java/com/kawunus/habitu/di/InteractorModule.kt
+++ app/src/main/java/com/kawunus/habitu/di/InteractorModule.kt
@@ -1,7 +1,12 @@
 package com.kawunus.habitu.di
 
+import com.kawunus.habitu.diary.domain.api.NoteInteractor
+import com.kawunus.habitu.diary.domain.impl.NoteInteractorImpl
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val interactorModule = module {
 
+    singleOf(::NoteInteractorImpl) { bind<NoteInteractor>() }
 }
--- app/src/main/java/com/kawunus/habitu/di/RepositoryModule.kt
+++ app/src/main/java/com/kawunus/habitu/di/RepositoryModule.kt
@@ -1,7 +1,12 @@
 package com.kawunus.habitu.di
 
+import com.kawunus.habitu.diary.data.impl.NoteRepositoryImpl
+import com.kawunus.habitu.diary.domain.api.NoteRepository
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val repositoryModule = module {
 
+    singleOf(::NoteRepositoryImpl) { bind<NoteRepository>() }
 }
