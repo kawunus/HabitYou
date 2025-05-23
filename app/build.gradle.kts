d6c12ffd22e63e357a20b81e302ed90413bb4032
kawunus <s.markovtsev@gmail.com>
2025-05-23 18:57:48 +0300

    Feat: Add Bottom Navigation Bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,7 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
+
+    // Navigation
+    implementation(libs.androidx.navigation.compose)
 }
