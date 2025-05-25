596fbeba97af2aa09f19d6b9b78abb73eda93d62
kawunus <s.markovtsev@gmail.com>
2025-05-25 16:07:20 +0300

    Feat: Add delete dialog
    This commit introduces a reusable dialog component for confirming delete actions.
    
    The following changes are included:
    - `EditDeleteDialog.kt`: Composable function for the delete confirmation dialog.
    - `DeleteDialogPreview.kt`: Preview functions for the delete dialog in different contexts (note and habit).
    - `strings.xml`: Added new string resources for the delete dialog.

---
+++ app/src/main/java/com/kawunus/habityou/core/presentation/dialog/delete/DeleteDialog.kt
@@ -0,0 +1,47 @@
+package com.kawunus.habityou.core.presentation.dialog.delete
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import com.kawunus.habityou.R
+
+@Composable
+fun EditDeleteDialog(
+    modifier: Modifier = Modifier,
+    @StringRes
+    titleResId: Int,
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit
+) {
+    AlertDialog(
+        modifier = modifier,
+        onDismissRequest = onDismiss,
+        icon = { Icon(Icons.Filled.Delete, contentDescription = null) },
+        title = { Text(text = stringResource(titleResId), textAlign = TextAlign.Center) },
+        text = {
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = stringResource(R.string.dialog_delete_cannot_undo),
+                textAlign = TextAlign.Center
+            )
+        },
+        confirmButton = {
+            Button(onClick = onConfirm) { Text(stringResource(R.string.dialog_delete_confirm)) }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text(stringResource(R.string.dialog_delete_cancel))
+            }
+        }
+    )
+}
---
+++ app/src/main/java/com/kawunus/habityou/core/presentation/dialog/delete/DeleteDialogPreview.kt
@@ -0,0 +1,23 @@
+package com.kawunus.habityou.core.presentation.dialog.delete
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.kawunus.habityou.R
+
+@Preview
+@Composable
+private fun EditDeleteDialogPreview_Note() {
+    EditDeleteDialog(
+        titleResId = R.string.dialog_delete_title_note,
+        onConfirm = {},
+        onDismiss = {})
+}
+
+@Preview
+@Composable
+private fun EditDeleteDialogPreview_Habit() {
+    EditDeleteDialog(
+        titleResId = R.string.dialog_delete_title_habit,
+        onConfirm = {},
+        onDismiss = {})
+}
