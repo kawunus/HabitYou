7dbb719e40202cb3695bf95346ae978c09a5a593
kawunus <s.markovtsev@gmail.com>
2025-05-26 02:00:59 +0300

    Feat: Add state logic for useful habits
    This commit introduces state management for the useful habits screen.
    
    Key changes:
    - Added `UsefulHabitsScreenState` to represent different states (Loading, Empty, Content).
    - Implemented `getData` function to fetch and process habits data.
    - Introduced `processResult` to transform raw data into `UsefulHabit` models, including streak and score calculation.
    - Added `renderState` function to update the UI based on the current state.

--- app/src/main/java/com/kawunus/habityou/ui/screens/usefulhabits/viewmodel/UsefulHabitsViewModel.kt
+++ app/src/main/java/com/kawunus/habityou/ui/screens/usefulhabits/viewmodel/UsefulHabitsViewModel.kt
@@ -7,6 +7,11 @@ import com.kawunus.habityou.domain.api.usecase.CalculateStreaksUseCase
 import com.kawunus.habityou.domain.api.usecase.GetUsefulHabitsWithEntriesUseCase
 import com.kawunus.habityou.domain.api.usecase.NewEntryUseCase
 import com.kawunus.habityou.domain.model.Entry
+import com.kawunus.habityou.domain.model.UsefulHabit
+import com.kawunus.habityou.domain.model.UsefulHabitWithEntries
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import java.time.Clock
 import java.time.LocalDate
@@ -19,10 +24,62 @@ class UsefulHabitsViewModel(
     private val getUsefulHabitsWithEntries: GetUsefulHabitsWithEntriesUseCase
 ) : ViewModel() {
 
+    private val _state = MutableStateFlow<UsefulHabitsScreenState>(UsefulHabitsScreenState.Loading)
+    val state: StateFlow<UsefulHabitsScreenState> = _state
 
     fun toggleEntry(habitId: Int, date: LocalDate) {
         viewModelScope.launch {
             newEntry(Entry(habitId = habitId, date = date))
         }
+    }
+
+    fun getData() {
+        viewModelScope.launch {
+            getUsefulHabitsWithEntries().collect { habitsList ->
+                processResult(habitsList)
+            }
+        }
+    }
+
+    private suspend fun processResult(habitsList: List<UsefulHabitWithEntries>) {
+        if (habitsList.isEmpty()) {
+            renderState(UsefulHabitsScreenState.Empty)
+        } else {
+            val habits = habitsList.map { habitWithEntries ->
+                val habit = habitWithEntries.habit
+                val entries = habitWithEntries.entries
+                val streak = calculateStreaks(habit.id).first().find { streak ->
+                    streak.endDate == LocalDate.now(clock) || streak.endDate == LocalDate.now(clock)
+                        .minusDays(1)
+                }
+                var score = calculateScore(habit.id).first()?.times(100)?.toInt()
+                if (score == 0) score = null
+                val completed = entries.filter { it.id != null }.map { it.date }.sortedDescending()
+                val completedByWeek =
+                    entries.filter { it.id == null }.map { it.date }.sortedDescending()
+                UsefulHabit(
+                    id = habit.id,
+                    name = habit.name,
+                    type = habit.frequency,
+                    streak = streak?.length,
+                    score = score,
+                    completed = completed,
+                    completedByWeek = completedByWeek,
+                )
+            }
+            renderState(
+                UsefulHabitsScreenState.Content(
+                    usefulHabits = habits,
+                    todaysDate = LocalDate.now(clock),
+                    showStreaks = true,
+                    showScore = true,
+                    showSubtitle = false
+                )
+            )
+        }
+    }
+
+    private fun renderState(state: UsefulHabitsScreenState) {
+        _state.value = state
     }
 }
